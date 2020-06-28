9f0803017b5e6c6bd53c920b2b90c94e5ccda20b
行比拓也 <imac@iMac.local>
2020-06-28 22:59:40 +0900

    todoリストの追加機能実装

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,13 +1,31 @@
 <template>
   <div>
+    <div class="form">
+      <form @submit.prevent="add">
+        <input v-model="name">
+        <button>ADD</button>
+      </form>
+    </div>
 
   </div>
 </template>
 
 <script>
   export default {
+    data(){
+      return {
+        name: '',
+        done: false
+      }
+    },
     created() {
       this.$store.dispatch('todos/init')
+    },
+    methods: {
+      add(){
+        this.$store.dispatch('todos/add', this.name)
+        this.name = ''
+      }
     }
   }
 </script>
